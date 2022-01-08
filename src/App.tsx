5ce08b49001fcd438053ab6e0feaa1e7cdfc71e0
Bruno Braga <brunobragaw8t@gmail.com>
2022-01-08 14:31:25 +0000

    Install multiple modules required for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap';
+import Home from './components/Home/Index';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
