1fe2d2145dfc9bcfe3a0684d45b23acd5a81a12f
Bruno Braga <brunobragaw8t@gmail.com>
2022-01-08 15:57:58 +0000

    Create header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap';
+import Header from './components/partials/Header';
 import Home from './components/Home/Index';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/partials/Header.tsx
@@ -0,0 +1,43 @@
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-secondary">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src="/img/logos/logo.png" alt="Wishlist Logo" height="60" />
+        </Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="offcanvas offcanvas-end bg-secondary" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title text-light" id="offcanvasNavbarLabel">Wishlist</h5>
+
+            <button type="button" className="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">Home</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/my-list">My list</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">Login / Register</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
